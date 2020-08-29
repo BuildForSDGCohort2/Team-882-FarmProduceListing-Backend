197c4bf117284ca2e1442964b10aeb37130e7295
Austaino <c3ono32@yahoo.com.com>
2020-08-29 03:49:27 -0700

    Initial Project Version

---
+++ FarmPortal/FarmPortal/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FarmPortal project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FarmPortal.settings')
+
+application = get_wsgi_application()
