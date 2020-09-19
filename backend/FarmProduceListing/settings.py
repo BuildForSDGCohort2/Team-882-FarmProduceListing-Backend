8bfe14c2b1423e4697b1bceeb719bb419f6424fb
Augustine Emeka <c3ono32@yahoo.com>
2020-09-19 13:28:17 -0700

    update settings.py
    removed email, to let the user insert his email

--- backend/FarmProduceListing/settings.py
+++ backend/FarmProduceListing/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tclassified.com@gmail.com'
+EMAIL_HOST_USER = 'your email'
 EMAIL_HOST_PASSWORD = 'your pass key'
 EMAIL_USE_TLS = True
 # Password validation
