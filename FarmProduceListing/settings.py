7c66829058f0d40ec36b4ae195201618ccfe998b
Austaino <c3ono32@yahoo.com.com>
2020-10-09 06:30:27 -0700

    repush

--- FarmProduceListing/settings.py
+++ FarmProduceListing/settings.py
@@ -95,7 +95,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'your email'
-EMAIL_HOST_PASSWORD = 'your pass key'
+EMAIL_HOST_PASSWORD = 'your pass keyword'
 EMAIL_USE_TLS = True
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
---
+++ build/precache-manifest.65f74943ac66741cf7b0b3cc9a88e426.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c905fd29632b0da83fec21ba73005ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1b930d2cfa4b4a4c5c1",
+    "url": "/static/css/main.6d1439c2.chunk.css"
+  },
+  {
+    "revision": "86cbcb7d8f26065de13e",
+    "url": "/static/js/2.cebf9fc9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cebf9fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b930d2cfa4b4a4c5c1",
+    "url": "/static/js/main.ceb6c54b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "3cec182ff0819f82d458541dcad0bbd7",
+    "url": "/static/media/banner.3cec182f.png"
+  },
+  {
+    "revision": "1617c9f803b9c0a5b645911475f109bc",
+    "url": "/static/media/navbarnow.1617c9f8.png"
+  },
+  {
+    "revision": "bb7df16a8a73e39481808ceb626cb26d",
+    "url": "/static/media/produce.bb7df16a.jpeg"
+  }
+]);
